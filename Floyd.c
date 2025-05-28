606c3450bef9dc1eb05f74d472a26fedc6e27a7c
Krish <151437686+Krish-aj@users.noreply.github.com>
2025-05-29 03:19:47 +0530

    Update Floyd.c

--- Floyd.c
+++ Floyd.c
@@ -1,46 +1,24 @@
-/ Floyd using openmp
+// Floyd using openmp
 #include<stdio.h>
-#include<omp.h>
+
 
 void floyd(int n,int cam[20][20]);
 void printmat(int dist[20][20],int n);
-void readmat();
 
 void main()
 {
-  readmat();
-}
-
-void readmat()
-{
   int cam[20][20],n,j,i;
-   float start,end;
+  
   
   printf("enter the number of vertices\n");
    scanf("%d",&n);
   
   printf("enter the cost adjacency matrix\n");
    for(i=0;i<n;i++)
-    for(j=0;j<n;j++)
-      scanf("%d",&cam[i][j]);
+      for(j=0;j<n;j++)
+          scanf("%d",&cam[i][j]);
 
-  printf("entered cost adjacency matrix is\n");
-  
- for(i=0;i<n;i++)
- {
-   printf("\n");
-    for(j=0;j<n;j++)
-      printf("%d\t",cam[i][j]);
- }
-
- start=omp_get_wtime();
-
-floyd(n,cam);
-
- end=omp_get_wtime();
-
-printf("time taken to compute is %f\n",end-start);
-
+  floyd(n,cam);
 }
 
 void floyd(int n,int cam[20][20])
@@ -51,7 +29,7 @@ void floyd(int n,int cam[20][20])
     for(j=0;j<n;j++)
        dist[i][j]=cam[i][j];
 
- #pragma omp parallel for  
+   
  for(k=0;k<n;k++)
      for(i=0;i<n;i++)
       for(j=0;j<n;j++)
