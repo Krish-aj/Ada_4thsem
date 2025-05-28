7b6792a512ebcf08786d03db8cddbb5e1c0ab946
Krish <151437686+Krish-aj@users.noreply.github.com>
2025-05-28 21:02:25 +0530

    Create warshal.c

---
+++ warshal.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+void wrshl(int n,int a[20][20])
+{
+	int i,j,k,m1[20][20],m2[20][20];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+			{
+				m1[i][j]=a[i][j];
+			}
+	}
+	
+	for(k=0;k<n;k++)
+	{
+		for(i=0;i<n;i++)
+			{
+				for(j=0;j<n;j++)
+				{
+					m2[i][j]=(m1[i][j] || (m1[i][k] && m1[k][j]));
+				}
+			}
+			
+		for(i=0;i<n;i++)
+			{
+				for(j=0;j<n;j++)
+				{
+					m1[i][j]=m2[i][j];
+				}
+			}
+	}
+	
+	for(i=0;i<n;i++)
+			{
+				for(j=0;j<n;j++)
+				{
+					printf("%d\t",m2[i][j]);
+				}
+				printf("\n");
+			}
+}
+void main()
+{
+	int i,j,k,n,a[20][20];
+	printf("Enter the no of vertices\n");
+	scanf("%d",&n);
+	printf("Enter the AM\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+			{
+				printf("Enter Edge %d,%d\n",i+1,j+1);
+				scanf("%d",&a[i][j]);
+			}
+	}
+	
+	printf("The Matrix entered is\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+			{
+				printf("%d\t",a[i][j]);
+				
+			}
+			printf("\n");
+	}
+	printf("The transitive closure for the graph is\n");
+	wrshl(n,a);
+}
+
+
+	
